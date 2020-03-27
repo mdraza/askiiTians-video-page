f5a01a36b9f1c89174765c211724a0d36ac32cbf
mdraza <raza.uidev@gmail.com>
2020-03-27 12:44:12 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function iit11(){
+    document.getElementById("iit-11").style.display = 'block';
+    document.getElementById("iit-12").style.display = 'none';
+    document.getElementById('11btn').style.backgroundImage = 'linear-gradient(45deg, #fe5206, #fc8c21)';
+    document.getElementById('11btn').setAttribute("style", "background-image: linear-gradient(45deg, #fe5206, #fc8c21); color:#fff");
+    document.getElementById('12btn').setAttribute("style", "background-image: linear-gradient(45deg, #efefef, #efefef); color:#333");
+    
+}
+function iit12(){
+    document.getElementById("iit-11").style.display = 'none';
+    document.getElementById("iit-12").style.display = 'block';
+    document.getElementById('11btn').setAttribute("style", "background-image: linear-gradient(45deg, #efefef, #efefef); color:#333");
+    document.getElementById('12btn').setAttribute("style", "background-image: linear-gradient(45deg, #fe5206, #fc8c21); color:#fff");
+    
+}
+function neet11(){
+    document.getElementById("neet-11").style.display = 'block';
+    document.getElementById("neet-12").style.display = 'none';
+    document.getElementById('neet11btn').setAttribute("style", "background-image: linear-gradient(45deg, #fe5206, #fc8c21); color:#fff");
+    document.getElementById('neet12btn').setAttribute("style", "background-image: linear-gradient(45deg, #efefef, #efefef); color:#333");
+    
+}
+function neet12(){
+    document.getElementById("neet-11").style.display = 'none';
+    document.getElementById("neet-12").style.display = 'block';
+    document.getElementById('neet11btn').setAttribute("style", "background-image: linear-gradient(45deg, #efefef, #efefef); color:#333");
+    document.getElementById('neet12btn').setAttribute("style", "background-image: linear-gradient(45deg, #fe5206, #fc8c21); color:#fff");
+    
+}
