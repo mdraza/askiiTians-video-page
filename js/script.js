64a3674b5cc7239e806d554c42f06ccc0ba29d4d
mdraza <raza.uidev@gmail.com>
2020-04-01 13:51:27 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-$(document).ready(function(){
-    $(".view-more").click(function(){
-      $(".upcomming-video-list").toggle();
-    });
+$(document).ready(function () {
+  $(".view-more1").click(function () {
+    $(".upcomming-video-list").toggle();
+    $("i", this).toggleClass("fa fa-plus fa fa-minus");
   });
+  
+});
+
+
+$('#show-more-content').hide();
+$('#show-less').hide();
+
+$('#show-more').click(function () {
+  $('#show-more-content').show();
+  $('#show-less').show();
+  $(this).hide();
+});
+
+$('#show-less').click(function () {
+  $('#show-more-content').hide();
+  $('#show-more').show();
+  $(this).hide();
+});
